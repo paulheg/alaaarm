092b37efca7e902380c780cae3e81ac3d5cc2424
Paul <paul.hegenberg@gmail.com>
2021-11-12 14:25:39 +0100

    Reformating setup.sql and fixing a type in main

--- migration/1_setup.sql
+++ migration/1_setup.sql
@@ -1,44 +1,44 @@
 CREATE TABLE IF NOT EXISTS "USER" (
 	"id" BIGSERIAL,
-	"created_at"	timestamp,
-	"updated_at"	timestamp,
-	"deleted_at"	timestamp,
+	"created_at" timestamp,
+	"updated_at" timestamp,
+	"deleted_at" timestamp,
 	"username" TEXT,
 	"telegram_id" INTEGER,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "ALERT" (
-	"id"	BIGSERIAL,
-	"created_at"	timestamp,
-	"updated_at"	timestamp,
-	"deleted_at"	timestamp,
-	"name"	text,
-	"description"	text,
-	"owner_id"	integer,
-	"token"	text,
+	"id" BIGSERIAL,
+	"created_at" timestamp,
+	"updated_at" timestamp,
+	"deleted_at" timestamp,
+	"name" text,
+	"description" text,
+	"owner_id" integer,
+	"token" text,
 	PRIMARY KEY("id"),
 	CONSTRAINT "fk_ALERT_owner" FOREIGN KEY("owner_id") REFERENCES "USER"("id")
 );
 
 CREATE TABLE IF NOT EXISTS "ALERT_RECEIVER" (
-	"alert_id"	INTEGER,
-	"user_id"	INTEGER,
-    "created_at"	timestamp,
-	"updated_at"	timestamp,
-	"deleted_at"	timestamp,
+	"alert_id" INTEGER,
+	"user_id" INTEGER,
+	"created_at" timestamp,
+	"updated_at" timestamp,
+	"deleted_at" timestamp,
 	PRIMARY KEY("alert_id", "user_id")
 );
 
 CREATE TABLE IF NOT EXISTS "INVITE" (
-	"id"	BIGSERIAL,
-	"created_at"	timestamp,
-	"updated_at"	timestamp,
-	"deleted_at"	timestamp,
-	"alert_id"	integer,
-	"token"	text,
-	"one_time"	BOOLEAN,
-	"expiration"	timestamp,
+	"id" BIGSERIAL,
+	"created_at" timestamp,
+	"updated_at" timestamp,
+	"deleted_at" timestamp,
+	"alert_id" integer,
+	"token" text,
+	"one_time" BOOLEAN,
+	"expiration" timestamp,
 	CONSTRAINT "fk_INVITE_alert" FOREIGN KEY("alert_id") REFERENCES "ALERT"("id"),
 	PRIMARY KEY("id")
 );
